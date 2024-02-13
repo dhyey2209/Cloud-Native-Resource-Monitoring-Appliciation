d9edb3fee3454371ed75af6131e61bcc8c0258a4
Dhyey Patel <dhyey2209@gmail.com>
2024-02-13 11:33:49 -0500

    Fixed Spacing

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index(): 
-
     cpu_percent = psutil.cpu_percent(interval=0.1)
     mem_percent = psutil.virtual_memory().percent 
     Message = None 
@@ -14,6 +13,4 @@ def index():
     return render_template("index.html",cpu_percent = cpu_percent, mem_percent = mem_percent, message=Message) 
 
 if __name__ == '__main__': 
-    app.run(debug=True, host='0.0.0.0')
-    
-    
+    app.run(debug=True, host='0.0.0.0') 
--- ecr.py
+++ ecr.py
@@ -5,8 +5,5 @@ ecr_client = boto3.client('ecr')
 repository_name = 'my-cloud-native-repo'
 response = ecr_client.create_repository(repositoryName=repository_name)
 
-
 repository_uri = response['repository']['repositoryUri']
 print(repository_uri)
-
-
